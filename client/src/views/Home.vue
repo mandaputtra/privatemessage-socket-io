632baa0fe0b86f94a1db445682f0ea2ae30ca78b
mandaputtra <mandaputra8@gmail.com>
2019-01-02 12:44:42 +0800

    login register

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,9 +1,35 @@
 <template>
-  <div>
-      <input type="text" placeholder="email" v-model="email">
-      <input type="text" placeholder="password" v-model="password">
-      <button @click="login()">Login</button>
-  </div>
+<div class="flex flex-col w-full m-auto max-w-xs h-screen">
+  <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    <div class="mb-4">
+      <label class="block text-grey-darker text-sm font-bold mb-2" for="username">
+        Username
+      </label>
+      <input
+      v-model="email"
+      class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
+    </div>
+    <div class="mb-6">
+      <label class="block text-grey-darker text-sm font-bold mb-2" for="password">
+        Password
+      </label>
+      <input
+      v-model="password"
+      class="shadow appearance-none border border rounded w-full py-2 px-3 text-grey-darker mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
+    </div>
+    <div class="flex items-center justify-between">
+      <button @click="login" class="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+        Sign In
+      </button>
+      <a class="inline-block align-baseline font-bold text-sm text-blue hover:text-blue-darker" href="#">
+        Forgot Password?
+      </a>
+    </div>
+  </form>
+  <p class="text-center text-grey text-xs">
+    ©2018 Chat App. All rights reserved.
+  </p>
+</div>
 </template>
 
 <script>
--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -1,42 +1,88 @@
 <template>
-  <div>
-    <input type="text" placeholder="email" v-model="email">
-    <input type="text" placeholder="last name" v-model="lastname">
-    <input type="text" placeholder="first name" v-model="firstname">
-    <input type="text" placeholder="password" v-model="password">
-    <input type="text" placeholder="phone" v-model="phone">
-    <button @click="register()">Register</button>
+  <div class="flex flex-col w-full m-auto max-w-xs h-screen">
+    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <div class="mb-4">
+        <label class="block text-grey-darker text-sm font-bold mb-2" for="username">
+          Email
+        </label>
+        <input v-model="email" class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline"
+          id="username" type="text" placeholder="Username">
+      </div>
+      <div class="mb-4">
+        <label class="block text-grey-darker text-sm font-bold mb-2" for="username">
+          First Name
+        </label>
+        <input v-model="firstname" class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline"
+          id="username" type="text" placeholder="Username">
+      </div>
+      <div class="mb-4">
+        <label class="block text-grey-darker text-sm font-bold mb-2" for="username">
+          Last Name
+        </label>
+        <input v-model="lastname" class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline"
+          id="username" type="text" placeholder="Username">
+      </div>
+      <div class="mb-4">
+        <label class="block text-grey-darker text-sm font-bold mb-2" for="username">
+          Phone
+        </label>
+        <input v-model="phone" class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline"
+          id="username" type="text" placeholder="Username">
+      </div>
+      <div class="mb-6">
+        <label class="block text-grey-darker text-sm font-bold mb-2" for="password">
+          Password
+        </label>
+        <input v-model="password" class="shadow appearance-none border border rounded w-full py-2 px-3 text-grey-darker mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          id="password" type="password" placeholder="******************">
+      </div>
+      <div class="flex items-center justify-between">
+        <button class="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="button" @click="register()">
+          Register
+        </button>
+        <a class="inline-block align-baseline font-bold text-sm text-blue hover:text-blue-darker" href="#">
+          Forgot Password?
+        </a>
+      </div>
+    </form>
+    <p class="text-center text-grey text-xs">
+      ©2018 Chat App. All rights reserved.
+    </p>
   </div>
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      email: '',
-      password: '',
-      firstname: '',
-      lastname: '',
-      phone: ''
-    }
-  },
-  methods: {
-    register () {
-      let data = {
-        email: this.email,
-        password: this.password,
-        first: this.firstname,
-        last: this.lastname,
-        phone: this.phone
+  export default {
+    data() {
+      return {
+        email: '',
+        password: '',
+        firstname: '',
+        lastname: '',
+        phone: ''
       }
-      fetch(this.$baseURL + 'users/register', {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: { 'Content-Type': 'application/json' }
-      })
-        .then(response => console.log(response.token))
-        .catch(err => console.log('err -', err))
+    },
+    methods: {
+      register() {
+        let data = {
+          email: this.email,
+          password: this.password,
+          first: this.firstname,
+          last: this.lastname,
+          phone: this.phone
+        }
+        fetch(this.$baseURL + 'users/register', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          })
+          .then(response => console.log(response.token))
+          .catch(err => console.log('err -', err))
+      }
     }
   }
-}
+
 </script>
