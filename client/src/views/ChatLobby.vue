fc58129f037a91fc86cbe57d0d5ab0ccd8fbd18c
mandaputtra <mandaputra8@gmail.com>
2019-01-02 13:01:57 +0800

    contact loby

--- client/src/views/ChatLobby.vue
+++ client/src/views/ChatLobby.vue
@@ -1,13 +1,19 @@
 <template>
-<div>
-  <h1>List of user</h1>
-</div>
+  <div class="flex flex-col w-full m-auto max-w-xs h-screen">
+    <h2 class="mt-4 mb-4">Your Contact List</h2>
+    <contact-list/>
+  </div>
 </template>
 
 <script>
-export default {
+import ContactList from '../components/Contact'
 
+export default {
+  components: {
+    'contact-list': ContactList
+  }
 }
+
 </script>
 
 <style>
