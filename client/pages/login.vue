e754f5267b5c5f9144103f0c8817d80189e3c8de
mandaputtra <mandaputra8@gmail.com>
2019-01-08 11:56:56 +0800

    login page

---
+++ client/pages/login.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-layout align-center justify-center row fill-height>
+    <v-flex xs4>
+      <v-card>
+        <v-card-title class="headline"> Login </v-card-title>
+        <v-form class="mx-4 pb-4">
+          <v-text-field label="email"></v-text-field>
+          <v-text-field type="password" label="password"></v-text-field>
+          <v-btn flat block> login </v-btn>
+        </v-form>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  layout: 'partial'
+}
+</script>
+
+<style>
+
+</style>
