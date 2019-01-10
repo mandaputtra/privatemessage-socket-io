88c4358bfa9f12fd99cb4851c0e85b9c1f4c6a1b
mandaputtra <mandaputra8@gmail.com>
2019-01-10 17:15:05 +0800

    chat design

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,20 +1,38 @@
 <template>
-  <v-container grid-list-xl text-xs-center>
-    <v-layout row justify-center align-center wrap>
+  <v-container grid-list-xl>
+    <v-layout row align-center wrap>
       <v-flex xs12>
-        <v-card dark color="primary">
-          <v-card-text class="px-0">{{ name }}</v-card-text>
-          <v-spacer></v-spacer>
-          <v-btn flat block @click="logout()">Logout </v-btn>
-        </v-card>
+        <v-layout row wrap>
+          <v-flex xs1 offset-xs2>
+            <v-avatar size="110px" class="elevation-2 ma-2">
+              <img src="https://pbs.twimg.com/profile_images/978369970773086208/IL1Ytuzu_400x400.jpg" alt="John Lieder">
+            </v-avatar>
+          </v-flex>
+          <v-flex class="mt-3 ml-5">
+            <p class="title mb-0">{{ name }}</p>
+            <p class="caption">{{ $store.state.userData.email }}</p>
+          </v-flex>
+        </v-layout>
+        <v-divider></v-divider>
+        <v-btn depressed small block @click="logout()">Log Out</v-btn>
       </v-flex>
-      <v-flex v-for="(item, index) in contact" :key="index" xs6>
-        <v-card dark color="secondary">
-          <v-card-text class="px-0">
-            {{ item.email }}
-          </v-card-text>
+      <v-flex v-for="(item, index) in contact" :key="index" xs4>
+        <v-card dark color="accent">
+          <v-layout row wrap>
+            <v-flex xs4 offset-xs1>
+              <v-avatar size="110px" class="elevation-4 ml-2">
+                <img src="https://pbs.twimg.com/profile_images/978369970773086208/IL1Ytuzu_400x400.jpg" alt="John Lieder">
+              </v-avatar>
+            </v-flex>
+            <v-flex xs6>
+              <v-card-text class="mt-2 pb-0 title"> {{ item.first + ' ' + item.last }}</v-card-text>
+              <v-card-text class="pt-0 caption">
+                {{ item.email }}
+              </v-card-text>
+            </v-flex>
+          </v-layout>
           <v-card-actions>
-            <v-btn flat block @click="chatUser(item)">Message</v-btn>
+            <v-btn depressed block @click="chatUser(item)">Message</v-btn>
           </v-card-actions>
         </v-card>
       </v-flex>
